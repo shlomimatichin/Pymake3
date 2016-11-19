a62378164662924bd13fb20a3b5b97221e385845
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-19 19:52:32 +0100

    cleaned up template csc.py, improved pymake core

--- src/pymake.py
+++ src/pymake.py
@@ -12,13 +12,38 @@ import sys
 # GLOBALS
 #---------------------------------------
 
+# Exit code to exit with when done.
+exit_code = 0
+
+# Known targets that have been registered with the @target decorator.
 targets = {}
 
 #---------------------------------------
 # FUNCTIONS
 #---------------------------------------
 
-def find_sources(path, pattern=None):
+def depends_on(*targets):
+    """
+    Registers dependencies for a make target.
+
+    :params targets: Names of the dependencies.
+    """
+
+    def decorator(func):
+        func.dependencies = targets
+        return func
+
+    return decorator
+
+def find_files(path, pattern=None):
+    """
+    Finds all files in the specified directory that match the specified
+    pattern.
+
+    :param path:    Path to a directory to search for files in.
+    :param pattern: Pattern to match filenames against.
+    """
+
     sources = []
 
     for s in os.path.listdir(path):
@@ -27,73 +52,130 @@ def find_sources(path, pattern=None):
         if os.path.isfile(s):
             if not pattern or fnmatch.fnmatch(s, pattern):
                 sources.append(s)
-            else:
-                sources.extend(find_sources(s, pattern))
+        else:
+            sources.extend(find_files(s, pattern))
 
     return sources
 
 def copy(srcpath, destpath, pattern=None):
+    """
+    Copies all files in the source path to the specified destination path.  The
+    source path can be a file, in which case that file will be copied as long as
+    it matches the specified mattern. If the source path is a directory, all
+    directories in it will be recursed and any files matching the specified
+    pattern will be copied.
+
+    :param srcpath:  Source path to copy files from.
+    :param destpath: Destination path to copy files to.
+    :param pattern:  Pattern to match filenames against.
+    """
+
     if os.path.isfile(srcpath):
         if pattern and not fnmatch.fnmatch(srcpath, pattern):
             return
 
         if os.path.exists(destpath):
-            srcmt = os.path.getmtime(srcpath)
-            destmt = os.path.getmtime(destpath)
-            if srcmt <= destmt:
+            # Only overwrite the file if the source is modified at a later time
+            # than the destination.
+            src_modified_time  = os.path.getmtime(srcpath)
+            dest_modified_time = os.path.getmtime(destpath)
+            if src_modified_time <= dest_modified_time:
                 return
 
         path, filename = os.path.split(destpath)
         trace('copying {} to {}', filename, path)
 
+        if not os.path.exists(path):
+            # Make sure all directories needed to copy the file exist.
+            os.makedirs(path)
+
         shutil.copyfile(srcpath, destpath)
         return
 
-    if not os.path.exists(destpath):
-        os.makedirs(destpath)
-
     for s in os.listdir(srcpath):
+        src  = os.path.join(srcpath , s)
         dest = os.path.join(destpath, s)
-        src = os.path.join(srcpath, s)
 
-        if os.path.isfile(s):
-            copy(src, dest, pattern)
-        else:
-            copy(src, dest, pattern)
+        copy(src, dest, pattern)
 
-def make(target=None):
-    if not target:
-        target = sys.argv[1] if len(sys.argv) > 1 else 'all'
+def create_dir(path):
+    """
+    Creates the specified directory if it does not exist.
+
+    :param path: Path of the directory to create.
+    """
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def make(target):
+    """
+    Attempts to make the specified target, making all its dependencies first.
+
+    :param target: The name of the target to make.
+    """
 
     if target not in targets:
-        trace('no such target {}', target)
+        trace('no such target: {}', target)
         return
 
-    func = targets[target]
+    make_func = targets[target]
 
-    for dep in func.dependencies:
-        make(dep)
+    if hasattr(make_func, 'dependencies'):
+        for dep in make_func.dependencies:
+            make(dep)
 
     trace()
-    func()
+    make_func()
+
+def pymake():
+    target = sys.argv[1] if len(sys.argv) > 1 else 'all'
+    make(target)
+    sys.exit(exit_code)
 
 def remove_dir(path):
+    """
+    Removes the specified directory if it exists.  All files inside the
+    directory will be removed.
+
+    :param path: Path of the directory to remove.
+    """
+
     if os.path.isdir(path):
         shutil.rmtree(path)
 
-def run_cmd(s, args):
-    subprocess.call([s] + args)
+def run_program(s, args):
+    """
+    Runs the specified program with the specified arguments.
 
-def target(*args):
-    def wrapper(func):
-        func.dependencies = args
-        name = func.__name__
-        targets[name] = func
-        return func
+    :param s:    Name of the program to run.
+    :param args: Program arguments.
+    """
 
-    return wrapper
+    global exit_code
+    exit_code = subprocess.call([s] + args)
+
+    return exit_code
+
+def target(func):
+    """
+    Marks a function as a make target. The function name will be its name.
+
+    :param func: Function to register as a target.
+    """
+
+    targets[func.__name__] = func
+
+    return func
 
 def trace(s=None, *args):
+    """
+    Displays the specified text, formatted with the specified arguments.
+
+    :param s:     Text to display.
+    :param args: Text formatting arguments.
+    """
+
     if not s:
         print
         return
--- src/templates/csc.py
+++ src/templates/csc.py
@@ -1,86 +1,53 @@
-#---------------------------------------
-# IMPORTS
-#---------------------------------------
-
-import os
-import sys
-
+import os, sys
 sys.path.insert(0, os.path.join('build/pymake'))
 from pymake import *
 
-#---------------------------------------
-# CONSTANTS
-#---------------------------------------
-
-# Name of the program.
 NAME = 'Program.exe'
 
-# Flags specifying how to compile the program.
-FLAGS = [
-#    '/debug',
-#    '/define:DEBUG',
-    '/nologo',
-    '/o',
-    '/platform:x64',
-    '/target:exe'
-]
+FLAGS = ['/nologo',
+         #'/debug',
+         #'/define:DEBUG',
+         '/o',
+         '/platform:x64',
+         '/target:exe']
 
-# Libraries needed to compile the program.
-LIBS = [
-    'PresentationCore.dll',
-    'System.IO.dll',
-    'System.Runtime.dll',
-    'WindowsBase.dll',
-]
+LIBS = ['PresentationCore.dll',
+        'System.IO.dll',
+        'System.Runtime.dll',
+        'WindowsBase.dll']
 
-# Library directories to search for libraries in.
-LIBDIRS = [
-    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\',
-    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\WPF',
-]
+LIBDIRS = [r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319',
+           r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\WPF']
 
-# Where to put the compiled binary files.
 BINDIR = 'bin'
-
-# Source code directory.
 SRCDIR = 'src'
 
-# Path to the C# compiler executable.
-CSC = 'C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\csc.exe'
+CSC = r'C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe'
 
-#---------------------------------------
-# FUNCTIONS
-#---------------------------------------
-
-@target('compile')
+@target
+@depends_on('compile')
 def all():
     pass
 
-@target()
+@target
 def clean():
-    if os.path.isdir(BINDIR):
-        remove_dir(BINDIR)
+    remove_dir(BINDIR)
 
-@target()
+@target
 def compile():
     if not os.path.exists(BINDIR):
         os.mkdir(BINDIR)
 
-    flags   = FLAGS
     libdirs = ['/lib:' + ','.join(LIBDIRS)]
-    libs    = ['/r:' + s for s in LIBS]
+    libs    = ['/r:' + lib for lib in LIBS]
     out     = ['/out:' + os.path.join(BINDIR, NAME)]
     sources = ['/recurse:' + os.path.join(SRCDIR, '*.cs')]
 
-    run_cmd(CSC, flags + libdirs + libs + out + sources)
+    run_program(CSC, FLAGS + libdirs + libs + out + sources)
 
-@target()
+@target
 def run():
     os.chdir(BINDIR)
     subprocess.call([TARGET])
 
-#---------------------------------------
-# SCRIPT
-#---------------------------------------
-
-make()
+pymake()
