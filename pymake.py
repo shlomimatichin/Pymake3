618248ed23e3d321126b5644f9112ac18d36d5cc
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2016-11-19 12:51:25 +0100

    initial commit

---
+++ pymake.py
@@ -0,0 +1,98 @@
+#---------------------------------------
+# IMPORTS
+#---------------------------------------
+
+import fnmatch
+import os
+import shutil
+import subprocess
+import sys
+
+#---------------------------------------
+# GLOBALS
+#---------------------------------------
+
+targets = {}
+
+#---------------------------------------
+# FUNCTIONS
+#---------------------------------------
+
+def find_sources(path, pattern=None):
+    sources = []
+
+    for s in os.path.listdir(path):
+        s = os.path.join(path, s)
+
+        if os.path.isfile(s):
+            if not pattern or fnmatch.fnmatch(s, pattern):
+                sources.append(s)
+            else:
+                sources.extend(find_sources(s, pattern))
+
+    return sources
+
+def copy(srcpath, destpath, pattern=None):
+    if os.path.isfile(srcpath):
+        if pattern and not fnmatch.fnmatch(srcpath, pattern):
+            return
+
+        if os.path.exists(destpath):
+            srcmt = os.path.getmtime(srcpath)
+            destmt = os.path.getmtime(destpath)
+            if srcmt <= destmt:
+                return
+
+        path, filename = os.path.split(destpath)
+        print trace('copying {} to {}', filename, path)
+
+        shutil.copyfile(srcpath, destpath)
+        return
+
+    if not os.path.exists(destpath):
+        os.makedirs(destpath)
+
+    for s in os.listdir(srcpath):
+        dest = os.path.join(destpath, s)
+        src = os.path.join(srcpath, s)
+
+        if os.path.isfile(s):
+            copy(src, dest, pattern)
+        else:
+            copy(src, dest, pattern)
+
+def make(target):
+    if target not in targets:
+        trace('no such target {}', target)
+        return
+
+    func = targets[target]
+
+    for dep in func.dependencies:
+        make(dep)
+
+    trace()
+    func()
+
+def remove_dir(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+
+def run_cmd(s, args):
+    subprocess.call([s] + args)
+
+def target(*args):
+    def wrapper(func):
+        func.dependencies = args
+        name = func.__name__
+        targets[name] = func
+        return func
+
+    return wrapper
+
+def trace(s=None, *args):
+    if not s:
+        print
+        return
+
+    print s.format(args)
---
+++ templates/csharp.py
@@ -0,0 +1,76 @@
+#---------------------------------------
+# IMPORTS
+#---------------------------------------
+
+import sys
+sys.path.insert(0, 'tools/pymake')
+from pymake import *
+
+#---------------------------------------
+# CONSTANTS
+#---------------------------------------
+
+CSC = 'C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\csc.exe'
+
+FLAGS = [
+#    '/debug',
+#    '/define:DEBUG',
+    '/nologo',
+    '/o',
+    '/platform:x64',
+    '/target:exe'
+]
+
+TARGET = 'Pong.exe'
+
+LIBS = [
+    'PresentationCore.dll',
+    'System.IO.dll',
+    'System.Runtime.dll',
+    'WindowsBase.dll',
+]
+
+BINDIR = 'bin'
+SRCDIR = 'src'
+
+LIBDIRS = [
+    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\',
+    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\WPF',
+]
+
+#---------------------------------------
+# FUNCTIONS
+#---------------------------------------
+
+@target('compile')
+def all():
+    pass
+
+@target()
+def clean():
+    if os.path.isdir(BINDIR):
+        remove_dir(BINDIR)
+
+@target()
+def compile():
+    if not os.path.exists(BINDIR):
+        os.mkdir(BINDIR)
+
+    flags   = FLAGS
+    libdirs = ['/lib:' + ','.join(LIBDIRS)]
+    libs    = ['/r:' + s for s in LIBS]
+    out     = ['/out:' + os.path.join(BINDIR, TARGET)]
+    sources = ['/recurse:' + os.path.join(SRCDIR, '*.cs')]
+
+    run_cmd(CSC, flags + libdirs + libs + out + sources)
+
+@target()
+def run():
+    os.chdir(BINDIR)
+    subprocess.call([TARGET])
+
+#---------------------------------------
+# SCRIPT
+#---------------------------------------
+
+make(sys.argv[1] if len(sys.argv) > 1 else 'all')
