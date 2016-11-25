490875f663b3d6e9f0814d3387222131f66fe3e9
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-25 15:38:15 +0100

    updated pong example

--- examples/csharp/pong.py
+++ examples/csharp/pong.py
@@ -8,44 +8,49 @@ import os, sys
 sys.path.insert(0, os.path.join('build', 'pymake2'))
 from pymake2 import *
 
-import csc
+from pymake2.template.csharp import csc
+from pymake2.template.util   import fswatcher
 
-@default_target
+# The configuration below depends on the backend used for the make process.  In
+# this case, we're using csc, which uses the options set below.
+conf={ 'name': 'Pong.exe',
+
+       'flags': ['/nologo',
+                 #'/debug',
+                 #'/define:DEBUG',
+                 '/optimize',
+                 '/target:winexe',
+                 '/platform:x64'],
+
+       'libdirs': csc.conf.libdirs + [ r'lib\SharpDX' ],
+
+       'libs': [ 'PresentationCore.dll',
+                 'System.IO.dll',
+                 'System.Runtime.dll',
+                 'WindowsBase.dll',
+
+                 'SharpDX.D3DCompiler.dll',
+                 'SharpDX.DXGI.dll',
+                 'SharpDX.Direct3D11.dll',
+                 'SharpDX.Mathematics.dll',
+                 'SharpDX.XAudio2.dll',
+                 'SharpDX.dll' ] }
+
+@default_target(conf=csc.conf)
 @depends_on('compile', 'content', 'libs')
 def all(conf):
     # The 'all' target doesn't need to do anything - it just depends on other
     # relevant targets to make.
     pass
 
-@target
+@target(conf=csc.conf)
 def content(conf):
     # Copy 'res' folder to 'Content' folder in the bin directory.
     copy('res', os.path.join(conf.bindir, 'Content'))
 
-@target
+@target(conf=csc.conf)
 def libs(conf):
     # Copy the SharpDX dll-files to the bin directory.
     copy(r'lib\SharpDX', conf.bindir, '*.dll')
 
-# The configuration below depends on the backend used for the make process.  In
-# this case, we're using csc, which uses the options set below.
-pymake2({ 'name'   : 'Pong.exe',
-
-         'flags'  : ['/target:winexe',
-                     #'/debug',
-                     #'/define:DEBUG',
-                     '/o',
-                     '/platform:x64'],
-         'libdirs': [r'lib\SharpDX'],
-
-         'libs'   : ['PresentationCore.dll',
-                     'System.IO.dll',
-                     'System.Runtime.dll',
-                     'WindowsBase.dll',
-
-                     'SharpDX.D3DCompiler.dll',
-                     'SharpDX.DXGI.dll',
-                     'SharpDX.Direct3D11.dll',
-                     'SharpDX.Mathematics.dll',
-                     'SharpDX.XAudio2.dll',
-                     'SharpDX.dll'] })
+pymake2(conf)
